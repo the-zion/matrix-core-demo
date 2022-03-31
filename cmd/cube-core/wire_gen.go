1e96759cf33f660499c95ffd4af9c4b96d2469df
liusiyuan <945212191@qq.com>
2022-03-31 22:42:07 +0800

    wire_go 文件更新

--- cmd/cube-core/wire_gen.go
+++ cmd/cube-core/wire_gen.go
@@ -25,9 +25,10 @@ func wireApp(confServer *conf.Server, confData *conf.Data, logger log.Logger) (*
 	if err != nil {
 		return nil, nil, err
 	}
-	greeterRepo := data.NewGreeterRepo(dataData, logger)
-	greeterUsecase := biz.NewGreeterUsecase(greeterRepo, logger)
-	realWorldService := service.NewRealWorldService(greeterUsecase)
+	userRepo := data.NewUserRepo(dataData, logger)
+	profileRepo := data.NewProfileRepo(dataData, logger)
+	userUseCase := biz.NewUserUseCase(userRepo, profileRepo, logger)
+	realWorldService := service.NewRealWorldService(userUseCase)
 	httpServer := server.NewHTTPServer(confServer, realWorldService, logger)
 	grpcServer := server.NewGRPCServer(confServer, realWorldService, logger)
 	app := newApp(logger, httpServer, grpcServer)
