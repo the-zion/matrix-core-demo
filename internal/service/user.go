49c688db25dee197c06ab5c129da4cf468868748
liusiyuan <945212191@qq.com>
2022-04-03 11:52:45 +0800

    service 完善 Login 与 Register调用逻辑

--- internal/service/user.go
+++ internal/service/user.go
@@ -3,17 +3,26 @@ package service
 import (
 	"context"
 	v1 "cube-core/api/realworld/v1"
+	"cube-core/internal/errors"
 )
 
 func (r *RealWorldService) Login(ctx context.Context, request *v1.LoginRequest) (*v1.UserReply, error) {
+	if len(request.User.Email) == 0 {
+		return nil, errors.NewHTTPError(422, "email", "can not be empty")
+	}
 	return &v1.UserReply{User: &v1.UserReply_User{
 		Username: "boom",
 	}}, nil
 }
 
 func (r *RealWorldService) Register(ctx context.Context, request *v1.RegisterRequest) (*v1.UserReply, error) {
+	u, err := r.uc.Register(ctx, request.User.Username, request.User.Email, request.User.Password)
+	if err != nil {
+		return nil, err
+	}
 	return &v1.UserReply{User: &v1.UserReply_User{
-		Username: "boom",
+		Username: u.Username,
+		Token:    u.Token,
 	}}, nil
 }
 
