e134bc0d172eca81b2328946bee8ba272cf3d4b4
liusiyuan <945212191@qq.com>
2022-04-04 15:47:45 +0800

    data层 完善用户注册与登录逻辑

--- internal/data/data.go
+++ internal/data/data.go
@@ -26,12 +26,21 @@ func NewData(c *conf.Data, logger log.Logger, db *gorm.DB) (*Data, func(), error
 }
 
 func NewDB(c *conf.Data) *gorm.DB {
-	db, err := gorm.Open(mysql.Open(c.Database.Dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(c.Database.Dsn), &gorm.Config{
+		DisableForeignKeyConstraintWhenMigrating: true,
+	})
 	if err != nil {
 		panic("failed to connect database")
 	}
 	if err := db.AutoMigrate(); err != nil {
 		panic(err)
 	}
+	InitDB(db) // 生产环境不建议这么做，建议单独把DB拿出来，不然每次启动都会被调用
 	return db
+}
+
+func InitDB(db *gorm.DB) {
+	if err := db.AutoMigrate(&User{}); err != nil {
+		panic(err)
+	}
 }
--- internal/data/user.go
+++ internal/data/user.go
@@ -2,14 +2,29 @@ package data
 
 import (
 	"context"
+	"github.com/go-kratos/kratos/v2/errors"
+	"gorm.io/gorm"
 
 	"cube-core/internal/biz"
 	"github.com/go-kratos/kratos/v2/log"
 )
 
+type FollowUser struct {
+	gorm.Model
+}
+
 type userRepo struct {
 	data *Data
 	log  *log.Helper
+}
+
+type User struct {
+	gorm.Model
+	Email        string `gorm:"size:500"`
+	Username     string `gorm:"size:500"`
+	Bio          string `gorm:"size:1000"`
+	Image        string `gorm:"size:1000"`
+	PasswordHash string `gorm:"size:500"`
 }
 
 // NewGreeterRepo .
@@ -21,10 +36,35 @@ func NewUserRepo(data *Data, logger log.Logger) biz.UserRepo {
 }
 
 func (r *userRepo) CreateUser(ctx context.Context, u *biz.User) error {
-	return nil
+	user := User{
+		Email:        u.Email,
+		Username:     u.Username,
+		Bio:          u.Bio,
+		PasswordHash: u.PasswordHash,
+	}
+	rv := r.data.db.Create(&user)
+	return rv.Error
 }
 
-func (r *userRepo) GetUserByEmail(ctx context.Context, email string) (*biz.User, error) {
+func (r *userRepo) GetUserByEmail(ctx context.Context, email string) (rv *biz.User, err error) {
+	u := new(User)
+	result := r.data.db.Where("email = ?", email).First(u)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, errors.NotFound("user", "not found by email")
+	}
+	if result.Error != nil {
+		return nil, err
+	}
+	return &biz.User{
+		Email:        u.Email,
+		Username:     u.Username,
+		Bio:          u.Bio,
+		Image:        u.Image,
+		PasswordHash: u.PasswordHash,
+	}, nil
+}
+
+func (r *userRepo) GetUserByUsername(ctx context.Context, username string) (*biz.User, error) {
 	return nil, nil
 }
 
